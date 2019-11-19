ecc96fc2744fbf667c6a670b30572a0af23943e8
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-19 11:41:08 +0530

    Wrote coiple of new test cases.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,12 +13,15 @@ void main() {
   print(myCurve.transform(0.0));
   print(myCurve.transform(1.0));
   //print(myCurve.transform(1.2));*/
+  Curve thisCurve = Curves.ease;
   test("Testing curve value", (){
 
       expect(MyCurve().transformInternal(0), 0.0);
+      expect(MyCurve().transformInternal(0.1), 0.0);
       expect(MyCurve().transformInternal(0.2), 0.0);
       expect(MyCurve().transformInternal(0.3), Curves.ease.transform(0.5));
       expect(MyCurve().transformInternal(0.4), 1.0);
       expect(MyCurve().transformInternal(0.5),  Curves.decelerate.transform(0.5));
+      expect(MyCurve().transformInternal(0.6), 0.0);
   });
 }
