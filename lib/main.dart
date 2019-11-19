64b37129b305d06ffcf0bdfb8b6e32d46d9808f7
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-11-19 11:38:57 +0530

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,14 @@ class _AnimatedDotsState extends State<AnimatedDots> {
 class CustomCurve extends Curve {
   @override
   double transformInternal(double t) {
-    if (t >= 0 && t < 0.2) {
+    if (t >= 0 && t <= 0.2) {
       return 0;
-    } else if (t >= 0.2 && t < 0.4) {
-      return Curves.ease.transform(t);
-    } else if (t >= 0.4 && t < 0.6) {
+    } else if (t > 0.2 && t < 0.4) {
+      return Curves.ease.transform(t+0.2);
+    }
+    else if (t ==0.4) {
+      return 1.0;
+    } else if (t >0.4 && t < 0.6) {
       return Curves.decelerate.transform(t);
     } else if (t >= 0.6 && t <= 1.0) {
       return 0;
